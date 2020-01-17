758192dfdfb6cff5b024709bf2e19b85c76c0cad
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-17 22:20:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,10 +18,10 @@ class Artist
     @@all
   end 
   
-  def self.songs
-    Song.all.select do |s|
-      s.artist == self 
-    end 
+  # def self.songs
+  #   Song.all.select do |s|
+  #     s.artist == self 
+  #   end 
   end 
   def self.find_or_create_by_name(name)
     self.all.detect {|artist| artist.name == name} || Artist.new(name)
