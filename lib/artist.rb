70eada80dbc53d92fcb27dac89b50e633a8833be
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-17 22:37:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,9 +13,10 @@ class Artist
     @@all
   end
   
-  def add_song(song)
-  @songs << song  
-  song.artist == self
+   def add_song(song)
+    @songs << song
+    song.artist = self
+    song
   end
   
   def songs 
