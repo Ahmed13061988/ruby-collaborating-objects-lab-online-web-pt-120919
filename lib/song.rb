3081c65f0fae47d4696a0c04570a57038bcc9483
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-17 22:42:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,13 @@ class Song
      self.artist = Artist.find_or_create_by_name(name)	
      artist.add_song(self)
   end 
-  
+  def self.new_by_filename(filename)	
+        artist = filename.split(" - ")[0]	
+        song = filename.split(" - ")[1]	
+        new_song = Song.new(song)	
+        new_song.artist=(Artist.new(artist))	
+        new_song	
+    end
 
 end 
   
