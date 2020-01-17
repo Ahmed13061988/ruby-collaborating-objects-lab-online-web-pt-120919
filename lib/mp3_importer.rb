4148f67179b69a93d71188f198e1979fb3fe4f54
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-17 22:13:17 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,13 +1,21 @@
-class Mp3Importer
-  attr_reader :import, :files
+class MP3Importer
+  attr_accessor :path
 
-  def initialize(import)
-    @import = import
-    @files = Dir.entries(path).grep(/.*\.mp3/)
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
   end
 
   def import
-    @files.each {|file| Song.new_by_filename(file)}
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
   end
-
-end 
+end
